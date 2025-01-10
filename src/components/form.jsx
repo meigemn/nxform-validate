2b1e39b24f9fb1aa33fef20f05056dbe39475cc5
José A. Muñoz Jiménez <jamj2000@gmail.com>
2025-01-10 13:52:01 +0100

    html pattern

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -25,6 +25,8 @@ function Formulario() {
             <div className="flex justify-between px-4 py-2 rounded-md bg-slate-200">
                 <label htmlFor="nombre">Nombre:</label>
                 <input id="nombre" name="nombre" className="ring-2"
+                    // pattern="[A-Za-zÑñÁÉÍÓÚáéíóú]{1,5}"
+                    // title="Mínimo 1 letra, máximo 5 letras"
                     defaultValue={state.payload?.get("nombre") || ""}  // para recuperar el valor introducido previamente
                 />
             </div>
